b9a4b22338c0bb3605a4029a17acc12eb3000647
Ignacio <naxete7@hotmail.es>
2022-11-07 18:35:02 +0100

    modificaciones en index

--- index.js
+++ index.js
@@ -3,18 +3,15 @@ const sequelize = require('./db/db')
 const { Sequelize } = require('./models/index')
 const app = express()
 const db = require('./db/db')
-<<<<<<< HEAD
-const router = require('./router')
-=======
-    // const router = require('./router')
->>>>>>> 33ab128a953fa901c4f6d1c96f29c570c20395fe
+ <!-- const router = require('./router') -->
+
 
 
 const PORT = 3002
 
 //middleware
 app.use(express.json())
-    // app.use(router)
+    <!-- // app.use(router) -->
 
 
 //Conexión con el servidor
@@ -22,14 +19,13 @@ app.listen(PORT, () => {
     console.log(`Servidor arrancado en el puerto ${PORT}`)
 
     //con sync({force: true}) se sincroniza sequelize con nuestra DB, force hace que pueda sobreescribir tablas
-<<<<<<< HEAD
+
     //con authenticate no sobreescribe y es más ligero, pero también se sincroniza
 
-    db.sync({ force: true }).then(() => {
-=======
+    <!-- db.sync({ force: true }).then(() => { -->
+
     //con authenticate no sobreescribe pero también se sincroniza
     sequelize.authenticate().then(() => {
->>>>>>> 33ab128a953fa901c4f6d1c96f29c570c20395fe
         console.log("Conectados a la DB")
     }).catch(error => {
         console.log('Se ha producido un error: ' + error)
